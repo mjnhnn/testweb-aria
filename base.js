79b45401dc932f2886d484b64848d39c422ef070
mjnhnn <61675864+mjnhnn@users.noreply.github.com>
2021-08-13 16:02:15 +0700

    Add files via upload

---
+++ base.js
@@ -0,0 +1,25 @@
+//#region scroll
+if (window.screen.width > 1072) {
+    let lastScrollTop = 0;
+    window.onscroll = function() {
+        var st = window.pageYOffset || document.documentElement.scrollTop;
+        if (st > 100) {
+            if (st > lastScrollTop) {
+                // downscroll code
+                $(".header-bot").removeClass("fixed")
+            } else {
+                // upscroll code
+                $(".header-bot").addClass("fixed")
+            }
+        } else {
+            $(".header-bot").removeClass("fixed")
+        }
+        lastScrollTop = st <= 0 ? 0 : st;
+    }
+}
+//#endregion
+
+if (window.screen.width <= 1072) {
+    let heightHeader = $(".header-mobile").outerHeight()
+    $(".banner__slider").css("margin-top", `${heightHeader}px`)
+}
--- script-index.js
+++ script-index.js
@@ -1,111 +1,126 @@
-function icon__close__top__fixed() {
-    var element = document.getElementById("top-fixed");
-    element.classList.add("disappear");
-    var elementw = document.getElementById("header-bots");
-    elementw.classList.add("margin-top-0");
-}
-$('.slider-banner').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: false,
-    dots: false,
-    autoplay: false,
-    autoplaySpeed: 4000,
-    cssEase: 'linear',
-});
-$('.media-wrap').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: false,
-    dots: true,
-    autoplay: false,
-    autoplaySpeed: 4000,
-    cssEase: 'linear',
-});
-$('.Portfolio__content').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: false,
-    dots: true,
-    autoplay: false,
-    autoplaySpeed: 4000,
-    cssEase: 'linear',
-});
-$('.Agriya-News__content').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: false,
-    dots: true,
-    autoplay: false,
-    autoplaySpeed: 4000,
-    cssEase: 'linear',
-});
-$('.Featured-In__content__list-slider').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    infinite: false,
-    dots: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    cssEase: 'linear',
-    responsive: [{
-            breakpoint: 1126,
-            settings: {
-                slidesToShow: 2
-            }
-        },
-        {
-            breakpoint: 540,
-            settings: {
-                slidesToShow: 1
-            }
-        }
-    ]
-});
-// counters js
-// $(document).ready(function() {
+ //#region init slick
 
-//     var counters = $(".counters__number");
-//     var countersQuantity = counters.length;
-//     var counter = [];
+ $('.slider-banner').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     infinite: false,
+     dots: false,
+     autoplay: false,
+     autoplaySpeed: 4000,
+     cssEase: 'linear',
+ });
+ $('.media-wrap').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     infinite: false,
+     dots: true,
+     autoplay: false,
+     autoplaySpeed: 4000,
+     cssEase: 'linear',
+ });
+ $('.Portfolio__content').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     infinite: false,
+     dots: true,
+     autoplay: false,
+     autoplaySpeed: 4000,
+     cssEase: 'linear',
+ });
+ $('.Agriya-News__content').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     infinite: false,
+     dots: true,
+     autoplay: false,
+     autoplaySpeed: 4000,
+     cssEase: 'linear',
+ });
+ $('.Featured-In__content__list-slider').slick({
+     slidesToShow: 6,
+     slidesToScroll: 1,
+     infinite: false,
+     dots: false,
+     autoplay: true,
+     autoplaySpeed: 4000,
+     cssEase: 'linear',
+     responsive: [{
+             breakpoint: 1126,
+             settings: {
+                 slidesToShow: 2
+             }
+         },
+         {
+             breakpoint: 540,
+             settings: {
+                 slidesToShow: 1
+             }
+         }
+     ]
+ });
 
-//     for (i = 0; i < countersQuantity; i++) {
-//         counter[i] = parseInt(counters[i].innerHTML);
-//     }
+ /*
+  ** With Slick Slider Plugin : https://github.com/marvinhuebner/slick-animation
+  ** And Slick Animation Plugin : https://github.com/marvinhuebner/slick-animation
+  */
 
-//     var count = function(start, value, id) {
-//         var localStart = start;
-//         setInterval(function() {
-//             if (localStart < value) {
-//                 localStart++;
-//                 counters[id].innerHTML = localStart;
-//             }
-//         }, );
-//     }
+ // Init slick slider + animation
+ $('.slider').slick({
+     autoplay: true,
+     speed: 800,
+     lazyLoad: 'progressive',
+     arrows: true,
+     dots: false,
+     prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+     nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+ }).slickAnimation();
 
-//     for (j = 0; j < countersQuantity; j++) {
-//         count(0, counter[j], j);
-//     }
-// });
-// counters js
 
-$('.counters__number').counterUp({ delay: 10, time: 5000 });
 
-// $(document).on('click', '.bidding-menu-link', function() {
-//     $(this).addClass('bidding-menu-link--active').siblings().removeClass('bidding-menu-link--active')
-// })
+ $('.slick-nav').on('click touch', function(e) {
 
-function openbinding(evt, biddingname) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("bidding-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("bidding-menu-link");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" bidding-menu-link--active", "");
-    }
-    document.getElementById(biddingname).style.display = "block";
-    evt.currentTarget.className += " bidding-menu-link--active";
-}
-document.getElementById("defaultOpen-binding").click();
+     e.preventDefault();
+
+     var arrow = $(this);
+
+     if (!arrow.hasClass('animate')) {
+         arrow.addClass('animate');
+         setTimeout(() => {
+             arrow.removeClass('animate');
+         }, 1600);
+     }
+
+ });
+ //#endregion
+ //#region tabs js
+ function openCity(evt, cityName) {
+     var i, tabcontent, tablinks;
+     tabcontent = document.getElementsByClassName("tabcontent");
+     for (i = 0; i < tabcontent.length; i++) {
+         tabcontent[i].style.display = "none";
+     }
+     tablinks = document.getElementsByClassName("tabpanel-link__btn");
+     for (i = 0; i < tablinks.length; i++) {
+         tablinks[i].className = tablinks[i].className.replace(" tabpanel-link__btn--active", "");
+     }
+     document.getElementById(cityName).style.display = "block";
+     evt.currentTarget.className += " tabpanel-link__btn--active";
+ }
+ document.getElementById("defaultOpen").click();
+
+ //tabs js vertical
+ function openbinding(evt, biddingname) {
+     var i, tabcontent, tablinks;
+     tabcontent = document.getElementsByClassName("bidding-content");
+     for (i = 0; i < tabcontent.length; i++) {
+         tabcontent[i].style.display = "none";
+     }
+     tablinks = document.getElementsByClassName("bidding-menu-link");
+     for (i = 0; i < tablinks.length; i++) {
+         tablinks[i].className = tablinks[i].className.replace(" bidding-menu-link--active", "");
+     }
+     document.getElementById(biddingname).style.display = "block";
+     evt.currentTarget.className += " bidding-menu-link--active";
+ }
+ document.getElementById("defaultOpen-binding").click();
+ //#endregion
